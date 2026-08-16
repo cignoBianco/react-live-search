616f6e305371406e4c6675bc2a638d27ecde18c3
smiana123@gmail.com <smiana123@gmail.com>
2026-08-17 00:44:56 +0300

    add next & previous buttons.

--- src/components/CountriesList/CountriesList.tsx
+++ src/components/CountriesList/CountriesList.tsx
@@ -50,7 +50,21 @@ export const CountriesList = (props: CountriesListProps) => {
 
     return (
         <div>
-            <h1 className='text-primary'>Countries</h1>
+            <div className='d-flex justify-content-between'>
+                <h1 className='text-primary'>Countries</h1>
+                <Dropdown>
+                    <Dropdown.Toggle className='m-2' size="sm" variant="secondary" id="dropdown-basic">
+                        {countriesPerPage} elements per page
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => setCountriesPerPage(10)} href="#/action-1">10</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setCountriesPerPage(20)} href="#/action-2">20</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setCountriesPerPage(50)} href="#/action-3">50</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
             <ul className='list-group mb-2'>
                 {filteredCountries().map((c, index) => {
                     const countryName = 'name' in c ? c.name : c.names.common;
@@ -61,18 +75,11 @@ export const CountriesList = (props: CountriesListProps) => {
                     </li>
                 })}
             </ul>
-            <Dropdown>
-                <Dropdown.Toggle className='m-2' size="sm" variant="secondary" id="dropdown-basic">
-                    {countriesPerPage} elements per page
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => setCountriesPerPage(10)} href="#/action-1">10</Dropdown.Item>
-                    <Dropdown.Item onClick={() => setCountriesPerPage(20)} href="#/action-2">20</Dropdown.Item>
-                    <Dropdown.Item onClick={() => setCountriesPerPage(50)} href="#/action-3">50</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
             <Pagination paginationLength={paginationItems()} clickHandler={(pageNumber: number) => setCurrentPage(pageNumber)} />
+            <div className='d-flex justify-content-center gap-2 mt-2'>
+                <button className='btn btn-primary' disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous page</button>
+                <button className='btn btn-primary' disabled={currentPage === paginationItems()} onClick={() => setCurrentPage(currentPage + 1)}>Next page</button>
+            </div>
         </div>
     )
 }
